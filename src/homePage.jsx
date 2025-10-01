27cba5c292a8a3df32596e58929bd0b82c23e20e
Akash Chalwadi <akash@edot-solutions.com>
2025-10-01 10:05:25 +0530

    Wed Commit

--- src/homePage.jsx
+++ src/homePage.jsx
@@ -1,4 +1,8 @@
 import { useEffect, useState} from "react";
+import { FaBars } from "react-icons/fa";
+import { FaRegCalendarAlt } from "react-icons/fa"; // Font Awesome icon
+import { FaUser } from "react-icons/fa";         // User icon
+import { FiSettings } from "react-icons/fi";     // Settings icon
 import './homePage.css';
 import {db} from './firebaseConfig';
 import { collection, addDoc, getDocs, updateDoc, doc, deleteDoc } from "firebase/firestore";
@@ -6,6 +10,11 @@ function HomePage (){
 
     const [text, setText] = useState("");
     const [todo, setTodo] = useState([]);
+    const [sideBarOpen, setSidebarOpen] = useState(false);
+    const [showIcons, setShowIcons] = useState(false);
+    const [showSettingsMenu, setshowSettingsMenu] = useState(false);
+    const [backgroundColor, setBackgroundColor] = useState();
+    const [showColorPicker, setShowColorPicker] = useState(false);
 
     // useEffect(()=>{
     //     const savedItems = localStorage.getItem("todos");
@@ -75,8 +84,54 @@ function HomePage (){
         setTodo(todos);
 
     }
+
+    const menu = () => {
+        // debugger
+        setSidebarOpen(!sideBarOpen);
+        setShowIcons(!showIcons);
+    }
+
+    const chooseBackgroundColor = () => {
+        <>
+        <h5>Choose Your Background Color</h5>
+        <input type="color" value={color}></input>
+        setBackgroundColor(value)
+        </>
+    }
+
     return(
         <div className="container">
+            <div className="sideBar" style={{width: sideBarOpen ? "20%" : "30px", minWidth: sideBarOpen ? "150px" : "0", backgroundColor: sideBarOpen ? " rgb(5, 85, 155)" : "transparent", transition: "width 0.3s ease"}}>
+            <FaBars size={32} style={{ cursor: "pointer", color: "black" }}  
+            onClick= {menu}/>
+
+    
+            {showIcons && <>
+            <FaRegCalendarAlt size={32} style={{ cursor: "pointer", color: "black" }}  />
+
+            <FiSettings size={32} style={{ cursor: "pointer", color: "black" }} onClick={()=> setshowSettingsMenu(!showSettingsMenu)} />
+                {showSettingsMenu && (
+                  <div className="settings-submenu">
+                    <div className="changeColor" onClick={()=> setShowColorPicker(!showColorPicker) }>Change Background Color</div>
+                    {showColorPicker && <>
+                    <div>
+                        <input type="color" value={backgroundColor || "#fff"} onChange={(e) => setBackgroundColor(e.target.value)}></input>
+                    </div>
+                    </>}
+                     
+                    {/* <div className="changeColor" onClick={()=> chooseBackgroundColor}></div> */}
+                    <div>Preferences</div>
+                    <div>Logout</div>
+                  </div>    
+                )}
+
+            <FaUser size={32} style={{ cursor: "pointer", color: "black" }} />
+            </>
+            }
+            
+
+            </div>
+            <div className="mainContent">
             <h4 style={{color:"black"}}>Todo List App</h4>
             <div className="inputContainerdiv">
                 <input className='inputContainer' value={text} onChange={(e)=> setText(e.target.value)}
@@ -101,6 +156,7 @@ function HomePage (){
                     </tbody>
                 </table>
             </div>
+        </div>
         </div>
     )
 }
